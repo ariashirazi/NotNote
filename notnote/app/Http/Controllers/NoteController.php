2e4b4d5c2f10d6a7897b94cd0b92afaf16a9233c
ariashirazi <ariashirazi7@gmail.com>
2024-03-25 12:17:47 +0330

    Add edit(update) and delete option

--- notnote/app/Http/Controllers/NoteController.php
+++ notnote/app/Http/Controllers/NoteController.php
@@ -31,7 +31,19 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $data = $request->validate([
+
+            'note' => ['required','string']
+
+        ]);
+
+        $data['user_id'] = 1;
+
+        $note = Note::create($data);
+
+        return to_route('note.show',$note)->with('message','Note was created');
+
     }
 
     /**
@@ -56,6 +68,16 @@ class NoteController extends Controller
     public function update(Request $request, Note $note)
     {
 
+        //validate the data
+        $data = $request->validate([
+
+            'note' => ['required','string']
+
+        ]);
+
+        $note->update($data);
+
+        return to_route('note.show',$note)->with('message','Note was updated');
     }
 
     /**
@@ -63,6 +85,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        //
+        $note->delete();
+        return to_route('note.index')->with('message','Note was deleted');
+
     }
 }
--- notnote/resources/views/note/index.blade.php
+++ notnote/resources/views/note/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 <div class="note-container">
-    <a href="/note/create" class="new-note-btn">
+    <a href="{{ route('note.create') }}" class="new-note-btn">
         New Note
     </a>
     <div class="notes">
@@ -10,10 +10,14 @@
                  {{ Str::words($note->note,30) }}
             </div>
             <div class="note-buttons">
-                <a href="#" class="note-edit-button">Views</a>
-                <a href="#" class="note-edit-button">Edit</a>
-                <a href="#" class="note-delete-button">Delete</a>
-
+                <a href="{{ route('note.show',$note) }}" class="note-edit-button">Views</a>
+                <a href="{{ route('note.edit',$note) }}" class="note-edit-button">Edit</a>
+                <form action="{{ route('note.destroy', $note) }}"
+                method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="note-delete-button">Delete</button>
+            </form>
             </div>
         </div>
         @endforeach
